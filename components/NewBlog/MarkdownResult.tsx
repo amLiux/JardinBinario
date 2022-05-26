294e1f8ae974416f1c8980987c09834e857f26bf
Marcelo J <marceliux@tutamail.com>
2022-05-26 00:20:40 -0600

    added an unauthorized client for SSR, made some changes to styling like scrollbar, and adapted terminal, terminalheader and markdown result components and style to being able to render the read blog POC which is finished

--- components/NewBlog/MarkdownResult.tsx
+++ components/NewBlog/MarkdownResult.tsx
@@ -2,20 +2,22 @@ import React, { useContext, useEffect, useState } from 'react'
 import editorContext from '../context/editorContext';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { UserContext } from '../types/sharedTypes';
+import { BlogEntry, UserContext } from '../types/sharedTypes';
 
 type MarkdownRestulProps = {
-	preview: boolean;
-	userContext?: UserContext;
+	preview?: boolean;
+	context?: UserContext;
+	blogEntry?: BlogEntry;
 };
 
 
-export const MarkdownResult = ({ preview, userContext }: MarkdownRestulProps) => {
+export const MarkdownResult = ({ preview = false, context, blogEntry }: MarkdownRestulProps) => {
 	const { visualMarkdown, setVisualMarkdown, title: mainTitle, setMarkdownText } = useContext(editorContext);
 	const [title, setTitle] = useState<string>('');
+	const [toRender, setToRender] = useState<string>('');
 
 	useEffect(() => {
-		const titleToRemove = visualMarkdown.split('\n')[0];
+		const titleToRemove = visualMarkdown.split('\n')[0] || blogEntry?.markdown?.split('\n')[0] || '';
 
 		const generateUserInfo = (context: UserContext) => {
 			const { lastName, name, email } = context;
@@ -27,9 +29,13 @@ export const MarkdownResult = ({ preview, userContext }: MarkdownRestulProps) =>
 		};
 
 
-		if (preview && userContext) {
-			const { name, date } = generateUserInfo(userContext as any);
+		if (preview && context) {
+			const { name, date } = generateUserInfo(context as any);
 			const toSet = titleToRemove + " \n" + name + "\n" + date + "\n";
+
+			if(blogEntry) {
+				setToRender(blogEntry.markdown.replace(titleToRemove, toSet));
+			}
 
 			if (!title && mainTitle !== toSet) {
 				setTitle(toSet);
@@ -37,20 +43,20 @@ export const MarkdownResult = ({ preview, userContext }: MarkdownRestulProps) =>
 			}
 		} else {
 			const toRemove = visualMarkdown.split('\n')[1] + "\n" + visualMarkdown.split('\n')[2];
-			if (!userContext && toRemove.includes('>')) {
+			if (!context && toRemove.includes('>')) {
 				const toRemove = visualMarkdown.split('\n')[1] + "\n" + visualMarkdown.split('\n')[2];
 				const newValue = visualMarkdown.replace(toRemove, '');
 				setVisualMarkdown(newValue);
 				setMarkdownText('markdown', newValue)
 			}
 		}
-	}, [preview, visualMarkdown, userContext, setVisualMarkdown, title, mainTitle, setMarkdownText]);
+	}, [preview, visualMarkdown, context, setVisualMarkdown, title, mainTitle, setMarkdownText, blogEntry]);
 
 	return (
 		<>
-			<div className={`markdownResult ${preview ? 'preview' : 'writing'}`}>
+			<div className={`markdownResult ${preview ? 'preview' : 'writing'} ${blogEntry ? 'readMarkdown' : ''}`}>
 				<ReactMarkdown
-					remarkPlugins={[[remarkGfm, { singleTilde: false }]]}>{visualMarkdown}</ReactMarkdown>
+					remarkPlugins={[[remarkGfm, { singleTilde: false }]]}>{toRender || visualMarkdown}</ReactMarkdown>
 			</div>
 		</>
 	)
