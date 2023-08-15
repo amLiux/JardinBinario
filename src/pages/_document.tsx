184ba35fb615ef0fdd95c80b05c64d110bab69ac
amLiux <marceliux@tutamail.com>
2023-08-14 18:25:12 -0600

    fixed crossOrigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,7 +24,7 @@ export default function Document() {
 				<meta name="theme-color" content="#ffffff" />
 				<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' integrity='sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
 				<link rel="preconnect" href="https://fonts.googleapis.com"/>
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<link href="https://fonts.googleapis.com/css2?family=Anuphan:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>
 
 			</Head>
