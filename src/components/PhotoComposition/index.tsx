bd09190fe5d3631271f44efdad9ceebd7e80b234
amLiux <marceliux@tutamail.com>
2024-03-02 11:40:48 -0600

    fixed image that looked a bit funky, I think this design it's a bit better and easier to mantain

--- src/components/PhotoComposition/index.tsx
+++ src/components/PhotoComposition/index.tsx
@@ -3,47 +3,65 @@ import Image from 'next/image';
 import imagen1 from '@/assets/imagen1.jpg';
 import imagen2 from '@/assets/imagen2.jpg';
 import imagen3 from '@/assets/imagen3.jpg';
+import imagen4 from '@/assets/imagen4.jpg';
+
 
 import photoCompositionStyles from './PhotoComposition.module.css';
+import { useState } from 'react';
 
 const toRender = [
 	{
 		image: imagen3,
 		alt: 'a desk with some work on branding design with an iPad',
+		overlayText: 'Diseño',
 	},
-	{ 
+	{
+		image: imagen4,
+		alt: 'a desk with some work on branding design with an iPad',
+		overlayText: 'Social Media',
+	},
+	{
 		image: imagen2,
 		alt: 'mobile application interface sketches',
+		overlayText: 'UI/UX'
 	},
 	{
 		image: imagen1,
 		alt: 'someone coding on a laptop with a cup on coffee and a plant on the desk',
+		overlayText: 'Software'
 	}
 ];
 
 export const PhotoComposition = () => {
-	const getComputedStyle = (number: number) => {
-		const propToCheck = `compositionPhoto--p${number}`;
-		return photoCompositionStyles?.[propToCheck] ? photoCompositionStyles[propToCheck] : '';
-	};
+	const [hoveringPic, setHoveringPic] = useState<Record<string, boolean>>({});
 
 	return (
 		<>
 			<div className={photoCompositionStyles.container}>
-				<div className={photoCompositionStyles.composition}>
-					{toRender.map(({image, alt}, idx) =>
-						<Image
-							key={idx}
-							className={`${photoCompositionStyles.compositionPhoto} ${getComputedStyle(idx + 1)}`}
-							alt={alt}
-							src={image}
-							width={400}
-							height={400}
-							objectFit='cover'
-						/>
-					)
-					}
-				</div>
+				{toRender.map(({ image, alt, overlayText }, idx) =>
+					<>
+						<div className={photoCompositionStyles.composition}>
+							<Image
+								onMouseOver={() => setHoveringPic({ [idx]: true })}
+								onMouseLeave={() => setHoveringPic({ [idx]: false })}
+								key={idx}
+								className={photoCompositionStyles.compositionPhoto}
+								alt={alt}
+								src={image}
+								width={400}
+								height={400}
+								objectFit='cover'
+							/>
+							<div className={photoCompositionStyles.overlay}>
+								<div className={`${photoCompositionStyles.overlayText} ${hoveringPic[idx] ? 'transition duration-500 opacity-0' : ''}`}>
+									{overlayText}
+								</div>
+							</div>
+						</div>
+
+					</>
+				)
+				}
 			</div>
 		</>
 	);
