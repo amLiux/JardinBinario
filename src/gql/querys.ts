3757347e9f44077a45560e356e085e0c72f47885
amLiux <marceliux@tutamail.com>
2024-09-10 21:26:45 -0600

    amliux/jardinbinario#182 added prettier, sorry for the delay lol

--- src/gql/querys.ts
+++ src/gql/querys.ts
@@ -1,213 +1,213 @@
 import { gql } from '@apollo/client';
 
 export const querys = {
-    REGISTER: gql`
-        mutation newUser($userInput: UserInput) {
-            newUser(userInput: $userInput) {
-                name,
-                lastName,
-                id
-            }
+  REGISTER: gql`
+    mutation newUser($userInput: UserInput) {
+      newUser(userInput: $userInput) {
+        name
+        lastName
+        id
+      }
+    }
+  `,
+  AUTHENTICATE: gql`
+    mutation authenticate($authInput: AuthInput) {
+      authenticate(authInput: $authInput) {
+        token
+      }
+    }
+  `,
+  GET_USER_INFO: gql`
+    query getUserInfo {
+      getUserInfo {
+        id
+        name
+        email
+        lastName
+        avatar
+      }
+    }
+  `,
+  FORGOT_PASSWORD_INIT: gql`
+    mutation initForgotPassword($email: String) {
+      initForgotPassword(email: $email)
+    }
+  `,
+  FORGOT_PASSWORD_FINISH: gql`
+    mutation finishForgotPassword($forgotPasswordInput: ForgotPasswordInput) {
+      finishForgotPassword(forgotPasswordInput: $forgotPasswordInput) {
+        name
+      }
+    }
+  `,
+  NEW_BLOG_ENTRY: gql`
+    mutation newBlogEntry($blogInput: BlogInput) {
+      newBlogEntry(blogInput: $blogInput) {
+        _id
+        author {
+          name
+          lastName
         }
-    `,
-    AUTHENTICATE: gql`
-        mutation authenticate($authInput: AuthInput) {
-            authenticate(authInput: $authInput) {
-                token
-            }
+        title
+      }
+    }
+  `,
+  GET_BLOG_BY_ID: gql`
+    query getSpecificBlogEntry($blogId: String!) {
+      getSpecificBlogEntry(blogId: $blogId) {
+        _id
+        title
+        markdown
+        createdAt
+        tags
+        sneakpeak
+        author {
+          name
+          lastName
+          email
+          avatar
         }
-    `,
-    GET_USER_INFO: gql`
-        query getUserInfo {
-            getUserInfo {
-                id,
-		        name,
-		        email,
-		        lastName,
-                avatar
-            }
+      }
+    }
+  `,
+  NEW_TICKET: gql`
+    mutation newTicket($ticketInput: TicketInput) {
+      newTicket(ticketInput: $ticketInput) {
+        id
+      }
+    }
+  `,
+  NEW_NEWSLETTER: gql`
+    mutation newNewsletterEntry($newsletterInput: NewsletterInput) {
+      newNewsletterEntry(newsletterInput: $newsletterInput) {
+        email
+      }
+    }
+  `,
+  GET_RECENT_BLOGS: gql`
+    query getRecentEntries {
+      getRecentEntries {
+        title
+        _id
+        markdown
+        createdAt
+        author {
+          name
+          lastName
+          email
+          avatar
         }
-    `,
-    FORGOT_PASSWORD_INIT: gql`
-        mutation initForgotPassword($email: String) {
-            initForgotPassword(email: $email)
+        views
+        shares
+        tags
+        sneakpeak
+        deleted
+      }
+    }
+  `,
+  GET_MOST_VIEWED_BLOGS: gql`
+    query getMostViewedEntries {
+      getMostViewedEntries {
+        title
+        _id
+        markdown
+        createdAt
+        author {
+          name
+          lastName
+          email
+          avatar
         }
-    `,
-    FORGOT_PASSWORD_FINISH: gql`
-        mutation finishForgotPassword($forgotPasswordInput: ForgotPasswordInput) {
-            finishForgotPassword(forgotPasswordInput: $forgotPasswordInput) {
-                name
-            }
+        views
+        shares
+        tags
+        sneakpeak
+        deleted
+      }
+    }
+  `,
+  GET_ALL_BLOGS_IDS: gql`
+    query getAllEntriesIds {
+      getAllEntriesIds {
+        _id
+      }
+    }
+  `,
+  UPDATE_BLOG_METRICS: gql`
+    mutation updateBlogMetrics($blogMetricsInput: BlogMetricsInput) {
+      updateBlogMetrics(blogMetricsInput: $blogMetricsInput)
+    }
+  `,
+  NEW_USER_METRIC: gql`
+    mutation newUserDetailsEntry($metricsInput: MetricsInput) {
+      newUserDetailsEntry(metricsInput: $metricsInput)
+    }
+  `,
+  GET_IMAGE_BY_PROMPT: gql`
+    query getImageByPrompt($prompt: String!) {
+      getImageByPrompt(prompt: $prompt) {
+        prompt
+        date
+        img {
+          buffer
+          contentType
         }
-    `,
-    NEW_BLOG_ENTRY: gql`
-        mutation newBlogEntry($blogInput: BlogInput) {
-            newBlogEntry(blogInput: $blogInput) {
-                _id,
-                author {
-                    name,
-                    lastName
-                },
-                title
-            }
+      }
+    }
+  `,
+  GET_ALL_IMAGES_OF_TODAY: gql`
+    query getAllImagesOfDay {
+      getAllImagesOfDay {
+        prompt
+        date
+        img {
+          buffer
+          contentType
         }
-    `,
-    GET_BLOG_BY_ID: gql`
-        query getSpecificBlogEntry($blogId:String!) {
-            getSpecificBlogEntry(blogId: $blogId) {
-                _id,
-                title,
-                markdown,
-                createdAt,
-                tags,
-                sneakpeak,
-                author {
-                    name
-                    lastName
-                    email
-                    avatar
-                },
-            }
+      }
+    }
+  `,
+  GET_ALL_BLOGS: gql`
+    query getAllEntries {
+      getAllEntries {
+        title
+        _id
+        markdown
+        createdAt
+        author {
+          name
+          lastName
+          email
+          avatar
         }
-    `,
-    NEW_TICKET: gql`
-        mutation newTicket($ticketInput: TicketInput) {
-            newTicket(ticketInput: $ticketInput) {
-                id
-            }
+        views
+        shares
+        tags
+        sneakpeak
+        deleted
+      }
+    }
+  `,
+  UPDATE_BLOG: gql`
+    mutation updateBlogEntry($blogInput: BlogInput) {
+      updateBlogEntry(blogInput: $blogInput) {
+        _id
+        author {
+          name
+          lastName
         }
-    `,
-    NEW_NEWSLETTER: gql`
-        mutation newNewsletterEntry($newsletterInput: NewsletterInput) {
-            newNewsletterEntry(newsletterInput: $newsletterInput) {
-                email
-            }
-        }
-    `,
-    GET_RECENT_BLOGS: gql`
-        query getRecentEntries {
-            getRecentEntries {
-                title,
-                _id,
-                markdown,
-                createdAt,
-                author {
-                    name
-                    lastName
-                    email,
-                    avatar
-                },
-                views,
-                shares,
-                tags,
-                sneakpeak,
-                deleted
-            }
-        }
-    `,
-    GET_MOST_VIEWED_BLOGS: gql`
-        query getMostViewedEntries {
-            getMostViewedEntries {
-                title,
-                _id,
-                markdown,
-                createdAt,
-                author {
-                    name
-                    lastName
-                    email,
-                    avatar
-                },
-                views,
-                shares,
-                tags,
-                sneakpeak,
-                deleted
-            }
-        }
-    `,
-    GET_ALL_BLOGS_IDS: gql`
-        query getAllEntriesIds {
-            getAllEntriesIds {
-                _id,
-            }
-        }
-    `,
-    UPDATE_BLOG_METRICS: gql`
-        mutation updateBlogMetrics($blogMetricsInput: BlogMetricsInput) {
-            updateBlogMetrics(blogMetricsInput: $blogMetricsInput)
-        }
-    `,
-    NEW_USER_METRIC: gql`
-        mutation newUserDetailsEntry($metricsInput: MetricsInput) {
-            newUserDetailsEntry(metricsInput: $metricsInput)
-        }
-    `,
-    GET_IMAGE_BY_PROMPT: gql`
-        query getImageByPrompt($prompt: String!) {
-            getImageByPrompt(prompt: $prompt) {
-                prompt,
-                date,
-                img {
-                    buffer,
-                    contentType,
-                },
-            }
-        }
-    `,
-    GET_ALL_IMAGES_OF_TODAY: gql`
-        query getAllImagesOfDay {
-            getAllImagesOfDay {
-                prompt,
-                date,
-                img {
-                    buffer,
-                    contentType,
-                },
-            }
-        }
-    `,
-    GET_ALL_BLOGS: gql`
-        query getAllEntries {
-            getAllEntries {
-                title,
-                _id,
-                markdown,
-                createdAt,
-                author {
-                    name
-                    lastName
-                    email,
-                    avatar
-                },
-                views,
-                shares,
-                tags,
-                sneakpeak,
-                deleted
-            }
-        }
-    `,
-    UPDATE_BLOG: gql`
-        mutation updateBlogEntry($blogInput: BlogInput) {
-            updateBlogEntry(blogInput: $blogInput) {
-                _id,
-                author {
-                    name,
-                    lastName
-                },
-                title
-            }
-        }
-    `,
-    DELETE_BLOG: gql`
-        mutation deleteBlogEntry($blogId: String) {
-            deleteBlogEntry(blogId: $blogId)
-        }
-    `,
-    RECOVER_BLOG: gql`
-        mutation recoverDeletedBlogEntry($blogId: String) {
-            recoverDeletedBlogEntry(blogId: $blogId)
-        }
-    `,
+        title
+      }
+    }
+  `,
+  DELETE_BLOG: gql`
+    mutation deleteBlogEntry($blogId: String) {
+      deleteBlogEntry(blogId: $blogId)
+    }
+  `,
+  RECOVER_BLOG: gql`
+    mutation recoverDeletedBlogEntry($blogId: String) {
+      recoverDeletedBlogEntry(blogId: $blogId)
+    }
+  `,
 };
--- src/types/sharedTypes.ts
+++ src/types/sharedTypes.ts
@@ -2,141 +2,142 @@ import { FormikHelpers, FormikValues } from 'formik';
 import { ReactNode, SyntheticEvent } from 'react';
 
 export type FormData = {
-	initialValues: FormikValues;
-	onSubmit: () => void;
+  initialValues: FormikValues;
+  onSubmit: () => void;
 };
 
 export type ForgotPasswordValues = {
-	email: string;
-	otp: string;
-	time: string;
-	newPassword: string;
-	confirmPassword: string | undefined;
+  email: string;
+  otp: string;
+  time: string;
+  newPassword: string;
+  confirmPassword: string | undefined;
 };
 
 type Author = {
-	name: string;
-	lastName: string;
-	email: string;
-	avatar: string;
+  name: string;
+  lastName: string;
+  email: string;
+  avatar: string;
 };
 
 export type BlogEntry = {
-	_id: string;
-	tags: string[];
-	title: string;
-	markdown: string;
-	createdAt: string;
-	author: Author;
-	views: number;
-	shares: number;
-	sneakpeak: string;
-	deleted?: boolean;
+  _id: string;
+  tags: string[];
+  title: string;
+  markdown: string;
+  createdAt: string;
+  author: Author;
+  views: number;
+  shares: number;
+  sneakpeak: string;
+  deleted?: boolean;
 };
 
 export type NewBlogEntryValues = {
-	tags: string[];
-	title: string;
-	markdown: string;
-	sneakpeak: string;
-	_id?: string;
+  tags: string[];
+  title: string;
+  markdown: string;
+  sneakpeak: string;
+  _id?: string;
 };
 
 export type NewTicketValues = {
-	companyName: string;
-	email: string;
-	description: string;
-	service: string[];
-	phoneNumber: string;
+  companyName: string;
+  email: string;
+  description: string;
+  service: string[];
+  phoneNumber: string;
 };
 
 export type NewsletterValues = {
-	email: string;
+  email: string;
 };
 
 export interface UserContext extends Author {
-	id?: string;
-	__typename?: string;
-};
+  id?: string;
+  __typename?: string;
+}
 
 type formikHelper = FormikHelpers<NewBlogEntryValues>['setFieldValue'];
 
 export type EditorContextType = {
-	setBlogTitle: any;
-	title: string;
+  setBlogTitle: any;
+  title: string;
 
-	setShowSneakpeak: (prop: boolean) => void;
+  setShowSneakpeak: (prop: boolean) => void;
 
-	setVisualMarkdown: (prop: string) => void;
-	setPreview: (prop: boolean) => void;
-	visualMarkdown: string;
+  setVisualMarkdown: (prop: string) => void;
+  setPreview: (prop: boolean) => void;
+  visualMarkdown: string;
 
-	tags: string[];
-	setTags: any;
+  tags: string[];
+  setTags: any;
 
-	setMarkdownText: any;
-	markdownText: string;
-	storeMarkdown: () => void;
+  setMarkdownText: any;
+  markdownText: string;
+  storeMarkdown: () => void;
 };
 
 export type RegisterFormValues = {
-	name: string;
-	lastName: string;
-	email: string;
-	password: string;
-	file: string;
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  file: string;
 };
 
 export type LoginFormValues = {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 };
 
 export type SignatureInfo = {
-    name: string;
-    position: string;
-    department: string;
-    phoneNumber: string;
-    email: string;
+  name: string;
+  position: string;
+  department: string;
+  phoneNumber: string;
+  email: string;
 };
 
 export type NavbarOptionsProps = {
-    setShowTags?: (e: SyntheticEvent) => void;
-    setPreview?: any;
-    storeMarkdown?: any;
-    setShowSneakpeak?: any;
-    read?: boolean;
-    editor?: boolean;
-}
-
-export type Options = {
-    tooltipText: string;
-    icon: ReactNode;
-    submitButton?: boolean;
-    onClick?: (e: SyntheticEvent) => void;
-    text?: string;
+  setShowTags?: (e: SyntheticEvent) => void;
+  setPreview?: any;
+  storeMarkdown?: any;
+  setShowSneakpeak?: any;
+  read?: boolean;
+  editor?: boolean;
 };
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    onClick?: (e: SyntheticEvent) => void;
-    form?: string;
-    type?: 'submit';
+export type Options = {
+  tooltipText: string;
+  icon: ReactNode;
+  submitButton?: boolean;
+  onClick?: (e: SyntheticEvent) => void;
+  text?: string;
+};
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick?: (e: SyntheticEvent) => void;
+  form?: string;
+  type?: 'submit';
 }
 
 export interface OptionProps {
-    buttonParams: ButtonProps;
-    option: Options;
-};
+  buttonParams: ButtonProps;
+  option: Options;
+}
 
 export type BasicObject = Record<string, string>;
 
 export type KeyOfBasicObject = keyof BasicObject;
 
 export interface Image {
-	prompt: string;
-	date: string;
-	img: {
-		buffer: string;
-		contentType: string;
-	}
+  prompt: string;
+  date: string;
+  img: {
+    buffer: string;
+    contentType: string;
+  };
 }
