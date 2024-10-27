e24f391e182528b9861e19e6957ea57ab81a0555
amLiux <marceliux@tutamail.com>
2024-10-27 00:17:26 -0700

    fixed revision

--- src/components/CookieBanner/index.tsx
+++ src/components/CookieBanner/index.tsx
@@ -6,21 +6,23 @@ import { Flexbox } from '../lib/Flexbox';
 
 const CookieBanner = () => {
 
-  const { handleAccept } = useCookie();
+  const { showBanner, handleAccept } = useCookie();
 
   return (
     <>
-      <Flexbox html='aside' justifyContent='space-around' alignItems='center' extraClass={cookieBannerStyles.container}>
-        <p className={cookieBannerStyles.cookieMessage}>
-          Utilizamos cookies para mejorar su experiencia en nuestro sitio web. Al continuar navegando por este sitio, usted acepta nuestro uso de cookies. Puedes familiarizarte con nuestra
-          <Link href='/privacy' passHref>
-            <span className={cookieBannerStyles.link}>Política de Privacidad.</span>
-          </Link>
-        </p>
-        <button onClick={() => { console.log('im being pressed') }} className={cookieBannerStyles.button}>
-          Aceptar
-        </button>
-      </Flexbox>
+      {showBanner && (
+        <Flexbox justifyContent='space-around' alignItems='center' extraClass={cookieBannerStyles.container}>
+          <p className={cookieBannerStyles.cookieMessage}>
+            Utilizamos cookies para mejorar su experiencia en nuestro sitio web. Al continuar navegando por este sitio, usted acepta nuestro uso de cookies. Puedes familiarizarte con nuestra
+            <Link href='/privacy' passHref>
+              <span className={cookieBannerStyles.link}>Política de Privacidad.</span>
+            </Link>
+          </p>
+          <button onClick={handleAccept} className={cookieBannerStyles.button}>
+            Aceptar
+          </button>
+        </Flexbox>
+      )}
     </>
   );
 };
